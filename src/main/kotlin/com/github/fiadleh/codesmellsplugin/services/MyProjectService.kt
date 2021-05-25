9f0197ff8a3ffaa2b2b8671333486c049cc1ed2c
GitHub Action <action@github.com>
2021-05-25 17:15:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/fiadleh/codesmellsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fiadleh/codesmellsplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fiadleh.codesmellsplugin.services
 
+import com.github.fiadleh.codesmellsplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
